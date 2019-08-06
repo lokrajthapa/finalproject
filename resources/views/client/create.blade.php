3ee2c58cdc6b2cf7d6233e777c808c6d577df8fc
lokrajthapa <lokrajthapa741@gmail.com>
2019-08-05 17:19:58 -0700

    first commit

---
+++ resources/views/client/create.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		#header{
+			background-color: transparent;
+			text-align: center;
+			color: rgb(0,244,100);
+			padding:5px;
+		        }
+
+
+		  #section{
+		  	width: 320px;
+		  	padding: 5px;
+		  	line-height:30px;
+		  	background-color:transparent;
+		  	justify-content: center;
+		  	color:rgb(100,100,245);
+		  }
+
+		  #body{ 
+		  	background-image:url('{{ URL::asset('img/lokrajhouse') }}');
+		  	background-attachment: cover;
+		  	
+		  
+
+
+		  }
+		
+
+	</style>
+
+</head>
+<body id="body">
+	
+
+
+
+	<h1 id="header">Hamro GharGhaderi  </h1> 
+	<br />
+	<br />
+	<div id="section"
+
+	<h2> About us </h2>
+
+	<p>This the platform where you can BUY and SELL your HOUSE and PLOT </p>
+      
+      </div>
+
+
+
+	<p></p>
+
+
+
+
+
+
+</body>
+</html>
